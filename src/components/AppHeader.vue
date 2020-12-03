bc344aac6399601bf0d645851b34e83d546eedf1
Ahmedloll <ahmed.alaa.fathy.96@gmail.com>
2020-12-04 01:13:19 +0200

    implemented the ui and styled it as a start

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>Hello world</div>
+  <header class="container">
+    <nav class="container">
+      <div class="container">
+        <img src="../assets/logo.png" />
+        <h3>NKN</h3>
+      </div>
+
+      <ul class="container">
+        <li class="container">
+          <a href="#">Home</a>
+        </li>
+        <li class="container">
+          <a href="#">Hotels</a>
+        </li>
+        <li class="container">
+          <a href="#">Promotions</a>
+        </li>
+        <li class="container">
+          <a href="#">Destinations</a>
+        </li>
+        <li class="container">
+          <a href="#">Voucher</a>
+        </li>
+        <li class="container">
+          <a href="#">About Us</a>
+        </li>
+      </ul>
+      <div class="container">
+        <a href="#">Sign Up</a>
+        <a class="login" href="#">log in</a>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -8,4 +40,52 @@ export default {
 };
 </script>
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+header {
+  background-color: #2d2d2d;
+  height: 60px;
+  padding: 10px 10%;
+}
+nav{
+  width: 100%;
+}
+img {
+  width: 45px;
+}
+h3 {
+  color: #ffffff;
+}
+ul {
+  list-style-type: none;
+  margin: 0%;
+  padding: 0;
+  flex-grow: 0.5;
+}
+li {
+  margin: 0 0.25%;
+}
+a {
+  color: #e4e4e4;
+  text-decoration: none;
+  margin: auto 5px;
+}
+li a {
+  color: #9f9f9f;
+}
+div a {
+  text-align: center;
+  margin: 20;
+}
+a.login {
+  width: 113px;
+  height: 36px;
+  border: 1px solid #e0ad1e;
+  border-radius: 5px;
+  line-height: 36px;
+}
 </style>
